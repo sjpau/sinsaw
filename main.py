991c147eb5ea1708bdfc4ddb94368954de73564b
sjpau <theonlymoby@protonmail.com>
2023-04-07 00:43:40 +0300

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+
+    # Do logical updates here.
+    # ...
+
+    screen.fill("purple")
+
+    # Render the graphics here.
+    # ...
+
+    pygame.display.flip()
+    clock.tick(60)
