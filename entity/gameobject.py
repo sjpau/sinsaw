90c512f374f1f17e0361c64b8145368facd6eb17
sjpau <theonlymoby@protonmail.com>
2023-05-18 16:00:33 +0300

    circular dependecy workaround

--- entity/gameobject.py
+++ entity/gameobject.py
@@ -1,13 +1,13 @@
 import pygame
-import entity.mapper as mapper
-import entity.asset as asset
-import entity.misc as misc
+import loader.asset as asset
+import misc
 from math import atan2, degrees
 
 class GameObject:
     def __init__(self, pos, xy, image, is_tile):
+        from loader.mapper import tile_size
         self.pos = pos
-        self.image = pygame.transform.scale(image, (mapper.tile_size, mapper.tile_size))
+        self.image = pygame.transform.scale(image, (tile_size, tile_size))
         self.default_image = self.image
         self.direction = pygame.math.Vector2(1, 0)
         self.direction_ptr = self.direction.copy()
@@ -16,8 +16,9 @@ class GameObject:
         else:
             self.rect = self.image.get_rect(topleft = xy)
     
-    def on_tile_index(self, layout, tiles):
-        return mapper.get_tile_index_from_layout(layout, tiles, self.pos)
+    def on_tile_index(self, layout, tiles):   
+        from loader.mapper import  get_tile_index_from_layout 
+        return get_tile_index_from_layout(layout, tiles, self.pos)
     
     def on_tile_collides_with(self, collider, layout, tiles):
         if self.on_tile_index(layout, tiles) == collider.on_tile_index(layout, tiles):
@@ -28,10 +29,11 @@ class GameObject:
         pass
     
     def shoot(self, layout, tiles, objects, weapon):
+        from loader.mapper import pos_to_xy, get_tile_index_from_layout, status
         if weapon == 1: # Fire extinguisher
             extinguisher_slice = misc.molotow_slice(layout, self.pos, h_area=2, v_area=2)
             for tile_pos in extinguisher_slice:
-                i = mapper.get_tile_index_from_layout(layout, tiles, tile_pos) # TODO split into separate functions
+                i = get_tile_index_from_layout(layout, tiles, tile_pos) # TODO split into separate functions
                 tiles[i].affected = 2 # Set in fog
         elif weapon == 2: # Pistol
             tiles_content = misc.slice_from_direction(layout, self.direction, self.pos)
@@ -39,30 +41,23 @@ class GameObject:
                 for game_obj in objects:
                     if game_obj.pos == [tile[0], tile[1]]:
                         game_obj.on_shot()
-                if mapper.status['indestructable'] in tiles[mapper.get_tile_index_from_layout(layout, tiles, [tile[0], tile[1]])].status:
+                if status['indestructable'] in tiles[get_tile_index_from_layout(layout, tiles, [tile[0], tile[1]])].status:
                     collide_pos = tiles_content[i]
                     break
         elif weapon == 4: # Molotow
             tiles_content = misc.slice_from_direction(layout, self.direction, self.pos)
             collide_pos = []
             for i, tile in enumerate(tiles_content):
-                tmp_tile_stat = tiles[mapper.get_tile_index_from_layout(layout, tiles, [tile[0], tile[1]])].status
-                if mapper.status['indestructable'] in tmp_tile_stat or mapper.status['destructable'] in tmp_tile_stat or mapper.status['breachable'] in tmp_tile_stat or mapper.status['unlockable'] in tmp_tile_stat:
+                tmp_tile_stat = tiles[get_tile_index_from_layout(layout, tiles, [tile[0], tile[1]])].status
+                if status['indestructable'] in tmp_tile_stat or status['destructable'] in tmp_tile_stat or status['breachable'] in tmp_tile_stat or status['unlockable'] in tmp_tile_stat:
                     collide_pos = tiles_content[i]
                     molotow_slice = misc.molotow_slice(layout, collide_pos)
                     for tile_pos in molotow_slice:
-                        i = mapper.get_tile_index_from_layout(layout, tiles, tile_pos)
+                        i = get_tile_index_from_layout(layout, tiles, tile_pos)
                         tiles[i].affected = 1 # Set on fire
                     break
-
-def pos_in_layout_borders(pos, layout):
-    MAX = len(layout)
-    MIN = 0
-    if pos[0] >= MIN and pos[0] < MAX and pos[1] >= 0 and pos[1] < MAX:
-        return True
-
-
-def update(self):
-    if self.direction != self.direction_ptr:
-        angle = degrees(atan2(self.direction[1], self.direction[0])) % 360
-        self.image = pygame.transform.scale(pygame.transform.rotate(self.default_image, angle), (mapper.tile_size, mapper.tile_size))
+    def update_object(self):
+        if self.direction != self.direction_ptr:
+            from loader.mapper import tile_size
+            angle = degrees(atan2(self.direction[1], self.direction[0])) % 360
+            self.image = pygame.transform.scale(pygame.transform.rotate(self.default_image, angle), (tile_size, tile_size))
